962cdc68f3eee90b5c73371088a73c575f05252d
Prankur Dixit <70831947+Prankur07-DS@users.noreply.github.com>
2025-05-30 20:32:49 +0530

    Update MTMathUILabel.h

--- iosMath/render/MTMathUILabel.h
+++ iosMath/render/MTMathUILabel.h
@@ -8,13 +8,13 @@
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
 //
-#import MTMathList.h
+
 
 @import CoreText;
 
 // Compatibility of iOS and Mac OS.
 #import "MTConfig.h"
-
+#import MTMathList.h
 #import "MTFont.h"
 
 #import "MTMathListDisplay.h"
